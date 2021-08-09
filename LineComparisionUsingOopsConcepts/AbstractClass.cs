2001b2c3db3d7dee5070747b59094cbda222e2a9
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-09 12:27:02 -0600

    Feat:Added line comparision using Oops concepts

---
+++ LineComparisionUsingOopsConcepts/AbstractClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparisionUsingOopsConcepts
+{
+    abstract class AbstractClass
+    {
+        public abstract double LengthOfLine1();
+        public abstract double LengthOfLine2();
+    }
+    class LineComparision : AbstractClass
+    {
+        public double x1, x2, y1, y2;
+        public double x3, x4, y3, y4;
+        public LineComparision()
+        {
+            x1 = 2;
+            x2 = 3;
+            y1 = 1;
+            y2 = 3;
+            x3 = 6;
+            x4 = 2;
+            y3 = 3;
+            y4 = 2;
+        }
+        public override double LengthOfLine1()
+        {
+            double length1 = x2 - x1;
+            double length2 = y2 - y1;
+            double LengthOfLine1 = Math.Sqrt(Math.Pow(length1, 2) + Math.Pow(length2, 2));
+            return LengthOfLine1;
+        }
+        public override double LengthOfLine2()
+        {
+            double length3 = x4 - x3;
+            double length4 = y4 - y3;
+            double LengthOfLine2 = Math.Sqrt(Math.Pow(length3, 2) + Math.Pow(length4, 2));
+            return LengthOfLine2;
+        }
+    }
+}
---
+++ LineComparisionUsingOopsConcepts/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LineComparisionUsingOopsConcepts
+{
+    class Program
+    { 
+       
+            
+            static void Main(string[] args)
+        {
+            LineComparision obj1 = new LineComparision();
+            LineComparision obj2 = new LineComparision();
+            double line1 = obj1.LengthOfLine1();
+            double line2 = obj2.LengthOfLine2();
+            Console.WriteLine("The length of first co-ordinates :" + line1);
+            Console.WriteLine("The length of second co-ordinates :" + line2);
+            if (line1 == line2)
+            {
+                Console.WriteLine("The length1 and length2 are equal");
+            }
+            else if (line1 > line2)
+            {
+                Console.WriteLine("The length1 is greater than length2");
+
+            }
+            else
+            {
+                Console.WriteLine("The length1 is less than length2");
+            }
+        }
+    }
+    }
+
+
